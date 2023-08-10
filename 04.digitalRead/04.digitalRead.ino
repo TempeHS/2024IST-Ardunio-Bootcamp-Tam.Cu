75838e8191aaafed685b0ee34d8dc9eb3041ee10
timthetamman <127055360+timthetamman@users.noreply.github.com>
2023-08-11 09:35:33 +1000

    Complete activity 4

--- 04.digitalRead/04.digitalRead.ino
+++ 04.digitalRead/04.digitalRead.ino
@@ -20,10 +20,39 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/04.digitalRead/Bootcamp-digitalRead.png
 */
 
-void setup() {
+static unsigned int myButton = 8;
+static unsigned int soundSensor = 7;
+static unsigned int lightSensor = 6;
+static unsigned int myLED = 5;
 
+void setup() {
+Serial.begin(9600);
+//Serial.println("Serial Monitor configures to 9600");
+//Serial.println("---------------------------");
+pinMode(myButton, INPUT);
+pinMode(soundSensor, INPUT);
+pinMode(lightSensor, INPUT);
+pinMode(myLED, OUTPUT);
 }
 
 void loop() {
+unsigned int btnVal = digitalRead(myButton);
+Serial.print("ButtonValue:");
+Serial.print(btnVal);
+Serial.print(",");
+unsigned int litVal = digitalRead(lightSensor);
+Serial.print("LightValue:");
+Serial.print(litVal);
+Serial.print(",");
+unsigned int sndVal = digitalRead(soundSensor);
+Serial.print("SoundValue:");
+Serial.print(sndVal);
+Serial.println();
+// ButtonValue:0,LightValue:1,SoundValue:0
 
+if (btnVal) {
+  digitalWrite(myLED, HIGH);
+  delay(500);
+}
+digitalWrite(myLED, LOW);
 }
